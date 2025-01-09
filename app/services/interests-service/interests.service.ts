fe048eab94468cd0c8a206c4f16d9f64b0960bd2
Josue Israel Reyes Barcenas <josue.reyesb@imss.gob.mx>
2025-01-08 18:20:43 -0600

    Validar que suban todas las carpetas

---
+++ app/services/interests-service/interests.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InterestsService {
+
+  accesoInterests = "interests experience running...";
+  
+  constructor() { }
+}
---
+++ environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebase:{
+        apiKey: "AIzaSyASNfkTULWBlYW5u-aphNwSD7F0Zz3xdEU",
+        authDomain: "my-cv-59b9c.firebaseapp.com",
+        projectId: "my-cv-59b9c",
+        storageBucket: "my-cv-59b9c.firebasestorage.app",
+        messagingSenderId: "1070849535754",
+        appId: "1:1070849535754:web:4085a12f3984526cc8d4f3"
+    }
+};
